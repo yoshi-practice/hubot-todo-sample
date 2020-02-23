837192c99beada495c62f0c8e2ede957bfc754fd
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-02-23 15:49:34 +0900

    Initial commit

---
+++ scripts/todo.js
@@ -0,0 +1,17 @@
+// Description:
+//   TODO を管理することができるボットです
+// Commands:
+//   ボット名 todo     - TODO を作成
+//   ボット名 done     - TODO を完了にする
+//   ボット名 del      - TODO を消す
+//   ボット名 list     - TODO の一覧表示
+//   ボット名 donelist - 完了した TODO の一覧表示
+
+'use strict';
+const todo = require('todo');
+
+//console.log(todo.list());
+
+module.exports = (robot) => {
+
+}
